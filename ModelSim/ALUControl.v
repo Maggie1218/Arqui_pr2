e8226f0264e8b0747f8a5d246423436946bea0a4
Maggie1218 <margarita.jaureguif@gmail.com>
2018-04-13 12:13:30 -0500

    Instruction J added

--- ModelSim/ALUControl.v
+++ ModelSim/ALUControl.v
@@ -27,6 +27,7 @@ localparam R_Type_ADD    = 9'b111_100000;
 localparam R_Type_SUB	 = 9'b111_100010;
 localparam R_Type_SRL	 = 9'b111_000010; 
 localparam R_Type_SLL	 = 9'b111_000000; 
+localparam R_Type_JR		 = 9'b111_001000; 
 
 localparam I_Type_ADDI   = 9'b110_xxxxxx;
 localparam I_Type_ORI    = 9'b101_xxxxxx;
@@ -34,6 +35,7 @@ localparam I_Type_ANDI   = 9'b001_xxxxxx;
 localparam I_Type_SW 	 = 9'b010_xxxxxx;
 localparam I_Type_LW 	 = 9'b011_xxxxxx;
 localparam I_Type_LUI	 = 9'b100_xxxxxx;
+localparam I_Type_Branch = 9'b000_xxxxxx;
 
 reg [3:0] ALUControlValues;
 wire [8:0] Selector;
@@ -56,8 +58,9 @@ always@(Selector)begin
 		I_Type_SW:		ALUControlValues = 4'b0011;
 		I_Type_LW:		ALUControlValues = 4'b0011;
 		I_Type_LUI:		ALUControlValues = 4'b0101;
+		I_Type_Branch: ALUControlValues = 4'b0100;
 		
-		
+
 		default: ALUControlValues = 4'b1001;
 	endcase
 end
